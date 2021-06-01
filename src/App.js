a589d6054c3f2aa4b0407a2ffcb4d1375753c107
BOKKA RAJESH <rajeshtnk4898@gmail.com>
2021-06-01 14:13:41 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Accordion, Button, Card } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h1 className="bg-primary text-light p-2">React Bootstrap</h1>
+      <h1 className="bg-dark text-light p-2">Carousel demo</h1>
 
-      <h2 className="bg-info text-dark p-2 mb-1">React Bootstrap Accordion</h2>
-
-      <Accordion defaultActiveKey="0">
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the React bootstrap </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm the React Bootstrap</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
+      <Carousel>
+        {[1, 2, 3].map((item, index) => (
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={"https://picsum.photos/100" + index}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 }
